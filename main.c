235bf28ce52fffe6a673f176e458cd4b79f39bff
EricTheRad <94642571+EricPhamTheRad@users.noreply.github.com>
2021-11-29 19:47:40 +0000

    Made function that prints menu

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+void PrintMenu();
+
 int main(void) {
   char input[250];
-
+  //gets and prints input
   printf("Enter a sample text:\n");
 
   fgets(input, 250, stdin);
 
-  printf("You entered: %s\n", input);
+  printf("\nYou entered: %s\n", input);
   
+  //print menu
+  PrintMenu();
+
   return 0;
+}
+
+void PrintMenu(){
+  printf("MENU\n");
+  printf("c - Number of non-whitespace characters\n");
+  printf("w - Number of words\n");
+  printf("f - Fix capitalization\n");
+  printf("r - Replace all !'s\n");
+  printf("s - Shorten spaces\n");
+  printf("q - Quit\n");
 }
